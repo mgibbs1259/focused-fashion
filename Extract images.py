fe337c092aae113338b1a4ce0736ca859fd0abd4
Jessica Fogerty <42726948+jfogerty18@users.noreply.github.com>
2019-11-04 16:28:29 -0500

    Add files via upload
    New Extract image script. This version works

--- Extract images.py
+++ Extract images.py
@@ -1,27 +1,56 @@
-import re
-import requests
-from bs4 import BeautifulSoup
-import os
+import time
+
 from selenium import webdriver
 
-site = 'https://www.gap.com/browse/category.do?cid=5664#pageId=0&department=136&mlink=5643,17458531,DP_DD_1_W_Jeans'
-directory = "gap_images/" #Relative to script location
 
-driver = webdriver.Chrome(r'/Users/jessicafogerty/Documents/chromedriver')
+def get_image_urls(driver):
+    images = driver.find_elements_by_tag_name("img")
+    image_links = []
+    for image in images:
+        image_url = (image.get_attribute("src"))
+        if image_url.endswith(".jpg"):
+            if image_url not in image_links:
+                image_links.append(image_url)
+                print(image_url)
+    return image_links
 
-driver.get(site)
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-img_tags = soup.find_all('img')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--disable-notifications")
+driver = webdriver.Chrome(options=chrome_options)
 
-urls = [img['src'] for img in img_tags]
 
-for url in urls:
-    #print(url)
-    filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+try:
+    url_list = ["https://bananarepublic.gap.com/browse/category.do?cid=5032&mlink=5001,,flyout_women_apparel_Sweaters&clink=15682852"]
+    image_urls = []
+    for url in url_list:
+        driver.get(url_list[0])
+        time.sleep(3)
+        driver.find_element_by_css_selector(".universal-modal__close-button").click()
+        time.sleep(3)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight*25/100);")
+        time.sleep(5)
+        image_urls += get_image_urls(driver)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight*50/100);")
+        time.sleep(5)
+        image_urls += get_image_urls(driver)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight*75/100);")
+        time.sleep(5)
+        image_urls += get_image_urls(driver)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(5)
+        image_urls += get_image_urls(driver)
+        driver.close()
+except:
+    driver.close()
 
-    with open(os.path.join(directory, filename.group(1)), 'wb') as f:
-        if 'http' not in url:
-            url = '{}{}'.format(site, url)
-        response = requests.get(url)
-        f.write(response.content)
+image_urls = (set(image_urls))
+#opens url, gets image, saves image.
+import requests
+i=1
+for url in image_urls:
+    img_data = requests.get(url).content
+    f = open(f"{i}sweater.jpg", "wb")
+    f.write(img_data)
+    f.close()
+    i+=1
