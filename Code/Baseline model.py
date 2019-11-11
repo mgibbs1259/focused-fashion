90f52d3554f5a03d32fc98cc30c4df10e0a66dca
Mary Gibbs <mgibbs1259@gmail.com>
2019-11-10 19:32:03 -0500

    output channel

--- Code/Baseline model.py
+++ Code/Baseline model.py
@@ -23,17 +23,26 @@ DROPOUT = 0.5
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=(3, 3), stride=1, padding=1)
-        self.convnorm1 = nn.BatchNorm2d(32) # Why does padding not affect this?
-        self.pool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=2) # What about padding here?
+        self.conv1 = nn.Conv2d(3, 32, (3, 3), stride=1, padding=1) # Output (n_examples, 32, 32, 32)
+        self.convnorm1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d((2, 2), stride=2) # Output (n_examples, 32, 16, 16)
 
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=(3, 3), stide=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, (3, 3), stide=1, padding=1) # Output (n_examples, 64, 16, 16)
         self.convnorm2 = nn.BatchNorm2d(64)
-        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2) # Output (n_examples, 64, 8, 8)
+
+        self.linear1 = nn.Linear(64*8*8, ?)
+        self.linear1_bn = nn.BatchNorm1d(?)
+        self.drop = nn.Dropout(DROPOUT)
+        self.linear2 = nn.Linear(?, ?)
+        self.act = torch.relu
+
 
     def forward(self, x):
         x = self.pool1(self.convnorm1(self.act(self.conv1(x))))
         x = self.pool2(self.convnorm2(self.act(self.conv2(x))))
+
+        return functional.sigmoid(x)
 
 
 DATA_PATH = "/home/ubuntu/Final-Project-Group8/Data/val_ann.csv"
