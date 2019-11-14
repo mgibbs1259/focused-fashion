28157e3b00867f18be66c577bb8c6d6d5b20b062
jfogerty18 <jfogerty18@gmail.com>
2019-11-14 15:04:21 -0500

    matching baseline

--- Code/JessicaBaseline.py
+++ Code/JessicaBaseline.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import torch
 import torch.optim as optim
@@ -15,10 +17,19 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
 
+# Change this
+MODEL_NAME = "model_number_1"
+
+
 LR = 5e-2
-N_EPOCHS = 1
-BATCH_SIZE = 500
+N_EPOCHS = 5
+BATCH_SIZE = 256
 DROPOUT = 0.5
+
+
+with open("{}.txt".format(MODEL_NAME), "w") as file:
+    file.write("MODEL_NAME: {}, LR: {}, N_EPOCHS: {}, BATCH_SIZE: {}, DROPOUT: {} \n".format(MODEL_NAME, LR, N_EPOCHS,
+                                                                                             BATCH_SIZE, DROPOUT))
 
 
 class CNN(nn.Module):
@@ -47,24 +58,27 @@ class CNN(nn.Module):
         return torch.sigmoid(x)
 
 
-TRAIN_DATA_PATH = "/home/ubuntu/Final-Project-Group8/Final-Project-Group8/train_ann.csv"
+TRAIN_DATA_PATH = "/home/ubuntu/Final-Project-Group8/train_ann.csv"
 TRAIN_IMG_DIR = "/home/ubuntu/Final-Project-Group8/Final-Project-Group8/Code/output_train"
 train_data_loader = load_images.create_data_loader(TRAIN_DATA_PATH, TRAIN_IMG_DIR, BATCH_SIZE)
 
 
-VAL_DATA_PATH = "/home/ubuntu/Final-Project-Group8/Final-Project-Group8/Code/val_ann.csv"
+VAL_DATA_PATH = "/home/ubuntu/Final-Project-Group8/val_ann.csv"
 VAL_IMG_DIR = "/home/ubuntu/Final-Project-Group8/Final-Project-Group8/Code/output_validation"
-val_data_loader = load_images.create_data_loader(VAL_DATA_PATH, VAL_IMG_DIR, batch_size=1000)
+val_data_loader = load_images.create_data_loader(VAL_DATA_PATH, VAL_IMG_DIR, batch_size=len(os.listdir(VAL_IMG_DIR)))
 
 
 model = CNN().to(device)
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
-criterion = nn.BCELoss()
+criterion = nn.BCEWithLogitsLoss()
 
 
 # Define epoch status
 epoch_status = tqdm.tqdm(total=N_EPOCHS, desc='Epoch', position=0)
 for epoch in range(N_EPOCHS):
+    # Write to file
+    with open("{}.txt".format(MODEL_NAME), "a") as file:
+        file.write("EPOCH: {} \n".format(epoch))
     # Update epoch status
     epoch_status.set_description('Epoch: {}'.format(epoch))
     # Training
@@ -78,7 +92,11 @@ for epoch in range(N_EPOCHS):
         train_loss.backward()
         optimizer.step()
         loss_train += train_loss.item()
-        print('Train Loss: {}'.format(train_loss))
+        # Write to file
+        with open("{}.txt".format(MODEL_NAME), "a") as file:
+            file.write('Batch Number: {}, Train Loss: {} \n'.format(train_idx, train_loss))
+        # Print status
+        print('Batch Number: {}, Train Loss: {}'.format(train_idx, train_loss))
     # Validation
     model.eval()
     with torch.no_grad():
@@ -88,16 +106,14 @@ for epoch in range(N_EPOCHS):
             cpu_tar = tar.cpu().numpy()
             cpu_val_output = np.where(val_output.cpu().numpy() > 0.5, 1, 0)
             f1 = f1_score(cpu_tar, cpu_val_output, average='micro')
-            print('F1 Score: {}'.format(f1))
+            # Write to file
+            with open("{}.txt".format(MODEL_NAME), "a") as file:
+                file.write('Validation F1 Score: {} \n'.format(f1))
+            # Print status
+            print('Validation F1 Score: {}'.format(f1))
     # Update epoch status
     epoch_status.update(1)
 
 
 # Save model
-# torch.save(model.state_dict(), '/home/ubuntu/Final-Project-Group8/baseline_model.pkl')
-# model = torch.load('/home/ubuntu/Final-Project-Group8/baseline_model.pkl')
-
-# https://stackoverflow.com/questions/42703500/best-way-to-save-a-trained-model-in-pytorch
-
-import os
-os.getcwd()
+torch.save(model.state_dict(), 'baseline_model.pt')
