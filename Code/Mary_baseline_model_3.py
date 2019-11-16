62154684030d002a6364cbd6d3c02eafc8a05a73
Mary Gibbs <mgibbs1259@gmail.com>
2019-11-15 21:36:30 -0500

    model 3

--- Code/Mary_baseline_model_3.py
+++ Code/Mary_baseline_model_3.py
@@ -63,9 +63,9 @@ torch.backends.cudnn.benchmark = False
 MODEL_NAME = "model_number_3"
 
 
-LR = 0.1
-N_EPOCHS = 5
-BATCH_SIZE = 1024
+LR = 0.01
+N_EPOCHS = 8
+BATCH_SIZE = 512
 
 
 with open("{}.txt".format(MODEL_NAME), "w") as file:
@@ -78,20 +78,24 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, (5, 5), stride=1, padding=1) # Output (98, 98)
         self.convnorm1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.AvgPool2d((2, 2), stride=2) # Output (49, 49)
 
-        self.conv2 = nn.Conv2d(32, 64, (5, 5), stride=1, padding=1) # Output (96, 96)
+        self.conv2 = nn.Conv2d(32, 64, (5, 5), stride=1, padding=1) # Output (47, 47)
         self.convnorm2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.AvgPool2d(kernel_size=(2, 2), stride=2) # Output (23, 23)
 
-        self.pool = nn.AvgPool2d(kernel_size=(2, 2), stride=2) # Output (48, 48)
+        self.conv3 = nn.Conv2d(64, 128, (5, 5), stride=1, padding=1) # Output (9, 9)
+        self.convnorm3 = nn.BatchNorm2d(128)
+        self.pool3 = nn.AvgPool2d(kernel_size=(2, 2), stride=2) # Output (4, 4)
 
-        self.linear = nn.Linear(64*48*48, 149)
+        self.linear = nn.Linear(128 * 4 * 4, 149)
 
         self.relu = torch.relu
 
     def forward(self, x):
-        x = self.convnorm1(self.relu(self.conv1(x)))
-        x = self.convnorm2(self.relu(self.conv2(x)))
-        x = self.pool(x)
+        x = self.pool1(self.convnorm1(self.relu(self.conv1(x))))
+        x = self.pool2(self.convnorm2(self.relu(self.conv2(x))))
+        x = self.pool3(self.convnorm3(self.relu(self.conv3(x))))
         x = self.linear(x.view(len(x), -1))
         return x
 
