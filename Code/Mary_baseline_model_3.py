58d0ea97574db4f4d6c23b08b10dca71229c25f0
Mary Gibbs <mgibbs1259@gmail.com>
2019-11-15 23:18:14 -0500

    model 3

--- Code/Mary_baseline_model_3.py
+++ Code/Mary_baseline_model_3.py
@@ -63,8 +63,8 @@ torch.backends.cudnn.benchmark = False
 MODEL_NAME = "model_number_3"
 
 
-LR = 0.01
-N_EPOCHS = 8
+LR = 0.001
+N_EPOCHS = 7
 BATCH_SIZE = 512
 
 
@@ -76,26 +76,35 @@ with open("{}.txt".format(MODEL_NAME), "w") as file:
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, (5, 5), stride=1, padding=1)
-        self.convnorm1 = nn.BatchNorm2d(32)
-        self.pool1 = nn.AvgPool2d((2, 2), stride=2)
+        self.conv1 = nn.Conv2d(3, 16, (3, 3), stride=1, padding=1)
+        self.convnorm1 = nn.BatchNorm2d(16)
+        self.pool1 = nn.MaxPool2d((2, 2), stride=2)
 
-        self.conv2 = nn.Conv2d(32, 64, (5, 5), stride=1, padding=1)
-        self.convnorm2 = nn.BatchNorm2d(64)
-        self.pool2 = nn.AvgPool2d(kernel_size=(2, 2), stride=2)
+        self.conv2 = nn.Conv2d(16, 32, (3, 3), stride=1, padding=1)
+        self.convnorm2 = nn.BatchNorm2d(32)
+        self.pool2 = nn.MaxPool2d((2, 2), stride=2)
 
-        self.conv3 = nn.Conv2d(64, 128, (5, 5), stride=1, padding=1)
-        self.convnorm3 = nn.BatchNorm2d(128)
-        self.pool3 = nn.AvgPool2d(kernel_size=(2, 2), stride=2)
+        self.conv3 = nn.Conv2d(32, 64, (3, 3), stride=1, padding=1)
+        self.convnorm3 = nn.BatchNorm2d(64)
+        self.pool3 = nn.MaxPool2d((2, 2), stride=2)
 
-        self.linear = nn.Linear(12800, 149)
+        self.conv4 = nn.Conv2d(64, 128, (3, 3), stride=1, padding=1)
+        self.convnorm4 = nn.BatchNorm2d(128)
+        self.pool4 = nn.MaxPool2d((2, 2), stride=2)
+
+        self.linear = nn.Linear(4608, 149)
 
         self.relu = torch.relu
 
     def forward(self, x):
-        x = self.pool1(self.convnorm1(self.relu(self.conv1(x))))
-        x = self.pool2(self.convnorm2(self.relu(self.conv2(x))))
-        x = self.pool3(self.convnorm3(self.relu(self.conv3(x))))
+        x = self.convnorm1(self.relu(self.conv1(x)))
+        x = self.pool1(x)
+        x = self.convnorm2(self.relu(self.conv2(x)))
+        x = self.pool2(x)
+        x = self.convnorm3(self.relu(self.conv3(x)))
+        x = self.pool3(x)
+        x = self.convnorm4(self.relu(self.conv4(x)))
+        x = self.pool4(x)
         x = self.linear(x.view(len(x), -1))
         return x
 
@@ -111,7 +120,7 @@ val_data_loader = create_data_loader(VAL_DATA_PATH, VAL_IMG_DIR, batch_size=len(
 
 
 model = CNN().to(device)
-optimizer = optim.Adam(model.parameters(), lr=LR)
+optimizer = optim.SGD(model.parameters(), lr=LR, momentum=0.9)
 criterion = nn.BCEWithLogitsLoss()
 
 
