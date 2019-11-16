c2cd3540efd5ec85bd498353f614142ff869d2bb
Mary Gibbs <mgibbs1259@gmail.com>
2019-11-15 21:09:55 -0500

    model 3

--- Code/Mary_baseline_model_3.py
+++ Code/Mary_baseline_model_3.py
@@ -45,7 +45,7 @@ class FashionDataset(Dataset):
 
 def create_data_loader(data_path, img_dir, batch_size):
      """Returns an image loader for the model."""
-     img_transform = transforms.Compose([transforms.Resize((120, 120), interpolation=Image.BICUBIC),
+     img_transform = transforms.Compose([transforms.Resize((100, 100), interpolation=Image.BICUBIC),
                                          transforms.ToTensor()])
      dataset = FashionDataset(data_path, img_dir, img_transform)
      loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=12, pin_memory=True)
@@ -64,44 +64,37 @@ MODEL_NAME = "model_number_3"
 
 
 LR = 0.01
-N_EPOCHS = 10
+N_EPOCHS = 5
 BATCH_SIZE = 1024
-DROPOUT = 0.50
 
 
 with open("{}.txt".format(MODEL_NAME), "w") as file:
-    file.write("MODEL_NAME: {}, LR: {}, N_EPOCHS: {}, BATCH_SIZE: {}, DROPOUT: {} \n".format(MODEL_NAME, LR, N_EPOCHS,
-                                                                                             BATCH_SIZE, DROPOUT))
+    file.write("MODEL_NAME: {}, LR: {}, N_EPOCHS: {}, BATCH_SIZE: {} \n".format(MODEL_NAME, LR, N_EPOCHS,
+                                                                                BATCH_SIZE))
 
 
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, (12, 12), stride=2, padding=1)
+        self.conv1 = nn.Conv2d(3, 32, (5, 5), stride=1, padding=1) # Output (98, 98)
         self.convnorm1 = nn.BatchNorm2d(32)
-        self.pool1 = nn.MaxPool2d((2, 2), stride=2)
 
-        self.conv2 = nn.Conv2d(32, 64, (8, 8), stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, (5, 5), stride=1, padding=1) # Output (96, 96)
         self.convnorm2 = nn.BatchNorm2d(64)
-        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
 
-        self.conv3 = nn.Conv2d(64, 128, (4, 4), stride=2, padding=1)
-        self.convnorm3 = nn.BatchNorm2d(128)
-        self.pool3 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.pool1 = nn.AvgPool2d(kernel_size=(2, 2), stride=2)
 
-        self.linear1 = nn.Linear(128*1*1, BATCH_SIZE)
-        self.linear1_bn = nn.BatchNorm1d(BATCH_SIZE)
-        self.drop = nn.Dropout(DROPOUT)
-        self.linear2 = nn.Linear(BATCH_SIZE, 149)
+        self.linear = nn.Linear(64*48*48, 149)
 
         self.relu = torch.relu
 
     def forward(self, x):
-        x = self.pool1(self.convnorm1(self.relu(self.conv1(x))))
-        x = self.pool2(self.convnorm2(self.relu(self.conv2(x))))
-        x = self.pool3(self.convnorm3(self.relu(self.conv3(x))))
-        x = self.drop(self.linear1_bn(self.relu(self.linear1(x.view(len(x), -1)))))
-        x = self.linear2(x)
+        x = self.convnorm1(self.relu(self.conv1(x)))
+        x = self.convnorm2(self.relu(self.conv2(x)))
+        x = self.convnorm3(self.relu(self.conv3(x)))
+        x = self.convnorm4(self.relu(self.conv4(x)))
+        x = self.pool(x)
+        x = self.linear(x.view(len(x), -1))
         return x
 
 
