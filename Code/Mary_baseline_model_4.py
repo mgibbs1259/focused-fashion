3fcd827ea7e48d9c750d4cea51acbcd729dbf631
Mary Gibbs <mgibbs1259@gmail.com>
2019-11-16 20:44:57 -0500

    model4

--- Code/Mary_baseline_model_4.py
+++ Code/Mary_baseline_model_4.py
@@ -7,7 +7,7 @@ import torch.optim as optim
 import tqdm
 from PIL import Image
 from ast import literal_eval
-from torchvision import transforms, models
+from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 from sklearn.preprocessing import MultiLabelBinarizer
 from torch import nn
@@ -51,8 +51,7 @@ def create_data_loader(data_path, img_dir, batch_size):
                                          transforms.RandomVerticalFlip(),
                                          transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3),
                                          transforms.CenterCrop(224),
-                                         transforms.ToTensor(),
-                                         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+                                         transforms.ToTensor()])
      dataset = FashionDataset(data_path, img_dir, img_transform)
      loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=12, pin_memory=True)
      return loader
@@ -66,11 +65,11 @@ torch.backends.cudnn.benchmark = False
 
 
 # Change this
-MODEL_NAME = "model_number_3"
+MODEL_NAME = "model_number_4"
 
 
 LR = 0.01
-N_EPOCHS = 10
+N_EPOCHS = 8
 BATCH_SIZE = 64
 
 
@@ -79,22 +78,41 @@ with open("{}.txt".format(MODEL_NAME), "w") as file:
                                                                                 BATCH_SIZE))
 
 
-class DenseModel(nn.Module):
+class CNN(nn.Module):
     def __init__(self):
-        super(DenseModel, self).__init__()
-        self.dense_model = models.densenet161(pretrained=True)
-        for param in self.parameters():
-            param.requires_grad = False
-        self.features = nn.Sequential(*list(self.dense_model.children())[:-1])
+        super(CNN, self).__init__()
+        self.relu = torch.relu
 
-        self.linear1 = nn.Linear(108192, 1024)
+        self.conv1 = nn.Conv2d(3, 32, (5, 5), stride=1, padding=1)
+        self.convnorm1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(32, 32, (5, 5), stride=1, padding=1)
+        self.convnorm2 = nn.BatchNorm2d(32)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+
+        self.conv3 = nn.Conv2d(32, 64, (5, 5), stride=1, padding=1)
+        self.convnorm3 = nn.BatchNorm2d(64)
+
+        self.conv4 = nn.Conv2d(64, 64, (5, 5), stride=1, padding=1)
+        self.convnorm4 = nn.BatchNorm2d(64)
+
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+
+        self.linear1 = nn.Linear(179776, 1024)
         self.linear1_bn = nn.BatchNorm1d(1024)
-        self.linear2 = nn.Linear(1024, 149)
+        self.linear2 = nn.Linear(1024, 256)
+        self.linear2_bn = nn.BatchNorm1d(256)
+        self.linear3 = nn.Linear(256, 149)
 
     def forward(self, x):
-        x = self.features(x)
-        x = self.linear1_bn(self.linear1(x.view(len(x), -1)))
-        x = self.linear2(x)
+        x = self.convnorm1(self.relu(self.conv1(x)))
+        x = self.pool1(self.convnorm2(self.relu(self.conv2(x))))
+        x = self.convnorm3(self.relu(self.conv3(x)))
+        x = self.pool2(self.convnorm4(self.relu(self.conv4(x))))
+        x = self.linear1_bn(self.relu(self.linear1(x.view(len(x), -1))))
+        x = self.linear2_bn(self.relu(self.linear2(x)))
+        x = self.linear3(x)
         return x
 
 
@@ -108,8 +126,8 @@ VAL_IMG_DIR = "/home/ubuntu/Final-Project-Group8/Data/output_validation"
 val_data_loader = create_data_loader(VAL_DATA_PATH, VAL_IMG_DIR, batch_size=len(os.listdir(VAL_IMG_DIR)))
 
 
-model = DenseModel().to(device)
-optimizer = optim.SGD(model.parameters(), lr=LR, momentum=0.9)
+model = CNN().to(device)
+optimizer = optim.Adam(model.parameters(), lr=LR)
 criterion = nn.BCEWithLogitsLoss()
 
 
