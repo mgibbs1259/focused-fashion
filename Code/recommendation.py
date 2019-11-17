c1e67c7d733dc55497178ee5c81a09941a2714ed
Mary Gibbs <mgibbs1259@gmail.com>
2019-11-16 23:55:16 -0500

    kmeans

---
+++ Code/recommendation.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+
+def KMeansClusteringElbowCurve(X):
+    """Shows an elbow curve plot to determine the appropriate number of k-means clusters."""
+    distorsions = []
+    for k in range(1, 20):
+        kmeans_model = KMeans(n_clusters=k)
+        kmeans_model.fit(X)
+        distorsions.append(kmeans_model.inertia_)
+    fig = plt.figure(figsize=(15, 5))
+    plt.plot(range(1, 4), distorsions)
+    plt.title('Elbow Curve')
+    plt.show()
+
+
+def KMeansClustering(X, clusters=10):
+    """Returns a pandas data frame containing the quote_dict and cluster label."""
+    kmeans_model = KMeans(n_clusters=clusters).fit(X)
+    predict_values = kmeans_model.predict(X)
+    return kmeans_model, predict_values
+
+
