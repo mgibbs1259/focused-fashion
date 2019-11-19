8cbf146732fd6dea2b7c54aecc5a0a7bf5febcf5
Mary Gibbs <mgibbs1259@gmail.com>
2019-11-19 13:30:22 -0500

    test

--- Code/test.py
+++ Code/test.py
@@ -5,9 +5,11 @@ import numpy as np
 import pandas as pd
 from torch import nn
 from PIL import Image
+from ast import literal_eval
 from torchvision import transforms, models
 from torch.utils.data import Dataset, DataLoader
-from keras.metrics import top_k_categorical_accuracy
+from sklearn.preprocessing import MultiLabelBinarizer
+# from keras.metrics import top_k_categorical_accuracy
 from sklearn.metrics import f1_score
 
 
@@ -20,37 +22,39 @@ class TestDataset(Dataset):
         Image directory path
         Image transformation
     """
-    def __init__(self, img_dir_path, img_transform, data_csv_path):
+    def __init__(self, data_csv_path, img_dir_path, img_transform):
         self.data_csv_path = data_csv_path
         self.img_dir_path = img_dir_path
         self.img_transform = img_transform
-        self.df = pd.read_csv(self.data_csv_path, header=0).reset_index(drop=True)
-        self.img_id = self.df['image_id']
-        self.img_label = self.df['image_label']
+        self.df = pd.read_csv(self.data_csv_path, header=0, names=['label_id', 'image_id']).reset_index(drop=True)
+        self.x_train = self.df['image_id']
+        self.mlb = MultiLabelBinarizer()
+        self.y_train = self.mlb.fit_transform(self.df['label_id'].apply(literal_eval))
 
     def __getitem__(self, index):
-        img = Image.open(os.path.join(self.img_dir_path, str(self.img_label[index])))
+        img = Image.open(os.path.join(self.img_dir_path, str(self.x_train[index]) + '.jpg'))
         img = img.convert('RGB')
         if self.img_transform is not None:
             img = self.img_transform(img)
-        return img
+        img_label = torch.from_numpy(self.y_train[index])
+        return img, img_label.float()
 
     def __len__(self):
-        return self.img_id.shape[0]
+        return self.x_train.shape[0]
 
 
 def create_data_loader(data_path, img_dir, batch_size):
     """Returns an image loader for the model."""
     img_transform = transforms.Compose([transforms.Resize((120, 120), interpolation=Image.BICUBIC),
                                         transforms.ToTensor()])
-    dataset = TestDataset(img_dir, img_transform, data_path)
+    dataset = TestDataset(data_path, img_dir, img_transform)
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=12, pin_memory=True)
     return loader
 
 
 TEST_DATA_PATH = "/home/ubuntu/Final-Project-Group8/Data/test_ann.csv"
 TEST_IMG_DIR = "/home/ubuntu/Final-Project-Group8/Data/output_test"
-test_data_loader = create_data_loader(TEST_DATA_PATH, TEST_IMG_DIR, batch_size=len(os.listdir(TEST_IMG_DIR)))
+test_data_loader = create_data_loader(TEST_DATA_PATH, TEST_IMG_DIR, batch_size=1000)
 
 
 # Load model
@@ -109,12 +113,12 @@ with torch.no_grad():
         # Print status
         print('Test BCELoss: {}'.format(bceloss_output))
 
-        top_5_categorical_accuracy = top_k_categorical_accuracy(tar, y_pred, k=5)
-        # Write to file
-        with open("test_{}.txt".format(MODEL_NAME), "a") as file:
-            file.write('Test Top 5 Categorical Accuracy Score: {} \n'.format(top_5_categorical_accuracy))
-        # Print status
-        print('Test Top 5 Categorical Accuracy Score: {}'.format(top_5_categorical_accuracy))
+        # top_5_categorical_accuracy = top_k_categorical_accuracy(tar, y_pred, k=5)
+        # # Write to file
+        # with open("test_{}.txt".format(MODEL_NAME), "a") as file:
+        #     file.write('Test Top 5 Categorical Accuracy Score: {} \n'.format(top_5_categorical_accuracy))
+        # # Print status
+        # print('Test Top 5 Categorical Accuracy Score: {}'.format(top_5_categorical_accuracy))
 
         cpu_tar = tar.cpu().numpy()
         cpu_val_output = y_pred.cpu().numpy()
