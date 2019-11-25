63dbf48f15a27ae7dcdc77f4d71b793b6f23363c
Mary Gibbs <mgibbs1259@gmail.com>
2019-11-24 22:06:02 -0500

    fix recommendation

--- Code/generate_recommendation.py
+++ Code/generate_recommendation.py
@@ -1,19 +1,37 @@
 import os
 
+import torch
 import numpy as np
 import pandas as pd
-
-import torch
 import torch.nn as nn
 from PIL import Image
 from annoy import AnnoyIndex
 from sklearn.neighbors import BallTree
-from sklearn.cluster import KMeans
 from torchvision import transforms, models
 from torch.utils.data import Dataset, DataLoader
 
 
-class RecommendationDataset(Dataset):
+EXAMPLE_DIR = "/home/ubuntu/Final-Project-Group8/Data/example_images/blazer"
+EXAMPLE_CSV_PATH = "/home/ubuntu/Final-Project-Group8/Code/blazer_example_image.csv"
+STORE_DIR = "/home/ubuntu/Final-Project-Group8/Code/banana_republic_images"
+STORE_CSV_PATH = "/home/ubuntu/Final-Project-Group8/Code/banana_republic_images.csv"
+
+
+def generate_image_mapping_csv(image_dir, csv_name):
+    """Returns a csv file mapping image_id and image_label for a given image directory."""
+    image_id = [i for i in range(len(os.listdir(image_dir)))]
+    image_label = os.listdir(image_dir)
+    image_dict = {"image_id": image_id, "image_label": image_label}
+    image_df = pd.DataFrame(image_dict)
+    image_df.to_csv("{}.csv".format(csv_name))
+
+
+# Generate image mapping csv files
+generate_image_mapping_csv(EXAMPLE_DIR, "blazer_example_image")
+generate_image_mapping_csv(STORE_DIR, "banana_republic_images")
+
+
+class FashionDataset(Dataset):
     """A dataset for the fashion images and fashion image labels.
 
     Arguments:
@@ -40,50 +58,22 @@ class RecommendationDataset(Dataset):
         return self.img_id.shape[0]
 
 
-def create_data_loader(data_path, img_dir, batch_size):
+def create_data_loader(img_dir, info_csv_path, batch_size):
      """Returns an image loader for the model."""
      img_transform = transforms.Compose([transforms.Resize(256),
                                          transforms.CenterCrop(224),
                                          transforms.ToTensor()])
-     dataset = RecommendationDataset(img_dir, img_transform, data_path)
-     loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=12, pin_memory=True)
-     return loader
+     img_dataset = FashionDataset(img_dir, img_transform, info_csv_path)
+     data_loader = DataLoader(img_dataset, batch_size=batch_size, shuffle=False, num_workers=12, pin_memory=True)
+     return data_loader
 
 
-# Define path to image of interest
-EXAMPLE_PATH = "/home/ubuntu/Final-Project-Group8/Code/Example"
-
-# Create a df mapping image of interest
-ex_image_id = [0]
-ex_image_label = ["example_img.jpg"]
-ex_image_dict = {"image_id": ex_image_id, "image_label": ex_image_label}
-ex_image_df = pd.DataFrame(ex_image_dict)
-ex_image_df.to_csv("example_image.csv")
-
-# Define path to image of interest csv
-EXAMPLE_CSV = "/home/ubuntu/Final-Project-Group8/Code/example_image.csv"
+# Create data loader
+example_loader = create_data_loader(EXAMPLE_DIR, EXAMPLE_CSV_PATH, batch_size=1)
+store_loader = create_data_loader(STORE_DIR, STORE_CSV_PATH, batch_size=64)
+store_df = pd.read_csv(STORE_CSV_PATH)
 
 
-# Define path to store images
-STORE_PATH = "/home/ubuntu/Final-Project-Group8/Code/Banana_Republic"
-
-# Create a df mapping Banana Republic Images
-image_id = [i for i in range(len(os.listdir(STORE_PATH)))]
-image_label = os.listdir(STORE_PATH)
-image_dict = {"image_id": image_id, "image_label": image_label}
-image_df = pd.DataFrame(image_dict)
-image_df.to_csv("banana_republic_images.csv")
-
-# Define path to store csv
-STORE_CSV = "/home/ubuntu/Final-Project-Group8/Code/banana_republic_images.csv"
-
-
-# Create data loaders for both
-example_loader = create_data_loader(EXAMPLE_CSV, EXAMPLE_PATH, batch_size=1)
-store_loader = create_data_loader(STORE_CSV, STORE_PATH, batch_size=64)
-
-
-# Load model
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
@@ -103,10 +93,13 @@ class CNN(nn.Module):
         return x
 
 
-def extract_feature_maps(x, model):
-    x = model.features(x)
-    x = model.linear(x)
-    return x
+def extract_feature_maps(data_loader, model):
+    """Extract the feature maps from the given model."""
+    with torch.no_grad():
+        for train_idx, features in enumerate(data_loader):
+            x = model.features(features)
+            feature_maps = model.linear(x)
+            return feature_maps
 
 
 model = CNN()
@@ -114,56 +107,33 @@ model.load_state_dict(torch.load("mobilenet_model.pt"))
 model.eval()
 
 
-def get_feature_maps(loader, model):
-    with torch.no_grad():
-        for train_idx, features in enumerate(loader):
-            features = extract_feature_maps(features, model)
-            return features
+# Get feature maps
+example_feature_maps = extract_feature_maps(example_loader, model)
+store_feature_maps = example_feature_maps(store_loader, model)
 
 
-# Get features for example_loader
-example_feature_maps = get_feature_maps(example_loader, model)
-print(example_feature_maps.size())
-
-
-# Get features for store_loader
-store_feature_maps = get_feature_maps(store_loader, model)
-print(store_feature_maps.size())
-
-
-# Annoy Approximate KNN
-# Store
-t = AnnoyIndex(store_feature_maps.size()[1], 'dot')  # Length of item vector that will be indexed
+# Approximate KNN
+# Index store
+store_item = AnnoyIndex(store_feature_maps.size()[1], 'dot')
 for i in range(store_feature_maps.size()[0]):
-    t.add_item(i, store_feature_maps[i])
-t.build(150) # 150 trees, more trees gives higher precision when querying
-t.save('store.ann')
-
-# Example
-u = AnnoyIndex(example_feature_maps.size()[1], 'dot')
-u.load('store.ann')
-recommendations = u.get_nns_by_item(0, 5)
-print(u.get_nns_by_item(0, 5, include_distances=True))
+    store_item.add_item(i, store_feature_maps[i])
+store_item.build(150) # More trees gives higher precision when querying
+store_item.save('store_items.ann')
+# Index example
+example_item = AnnoyIndex(example_feature_maps.size()[1], 'dot')
+example_item.load('store.ann')
+recommendations = example_item.get_nns_by_item(0, 5)
+print(example_item.get_nns_by_item(0, 5, include_distances=True))
 for recommendation in recommendations:
-    print(image_df['image_label'][recommendation])
+    print(store_df['image_label'][recommendation])
 
 
-# Sklearn KNN
-# https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.BallTree.html#sklearn.neighbors.BallTree
+# Scikit-learn KNN
 rng = np.random.RandomState(42)
 tree = BallTree(store_feature_maps)
 dist, ind = tree.query(example_feature_maps, k=5)
-print(ind) # indices of 5 closest neighbors
-print(dist) # distances to 5 closest neighbors
+print(ind) # Indices of 5 closest neighbors
+print(dist) # Distances to 5 closest neighbors
 for i in ind:
     for idx in i:
-        print(image_df['image_label'][idx])
-
-
-# # Sklearn KMeans
-# kmeans_df = image_df
-# kmeans_model = KMeans(n_clusters=15).fit(store_feature_maps)
-# kmeans_df['cluster_labels'] = kmeans_model.labels_
-# y = kmeans_model.predict(example_feature_maps)
-# print("Predicted example label: {}".format(int(y)))
-# print(kmeans_df[kmeans_df['cluster_labels'] == int(y)])
+        print(store_df['image_label'][idx])
